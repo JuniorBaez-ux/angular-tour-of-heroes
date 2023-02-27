ae655c044b0995ad1b49af59705d956ab326993a
Junior Baez <junior_baez@ucne.edu.do>
2023-02-27 12:17:38 -0400

    Continuation of the tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,17 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent {
 
-  heroes = HEROES;
-  selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
